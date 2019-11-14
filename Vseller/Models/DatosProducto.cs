c1a461829fc7846140ba5bb0b6d01c141c4dc3b8
FEDERICO ILAN KOZAK <44554540@comunidadort.edu.ar>
2019-11-14 09:10:21 -0300

    Clases

---
+++ Vseller/Models/DatosProducto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vseller.Models
+{
+    public class DatosProducto
+    {
+        private int _fkProducto;
+        private int _fkDetalle;
+        private string _descripcion;
+
+        public int fkProducto { get => fkProducto; set => _fkProducto = value; }
+        public int fkDetalle { get => fkDetalle; set => _fkDetalle = value; }
+        public string descripcion { get => descripcion; set => _descripcion = value; }
+
+        public DatosProducto(int a, int b, string c)
+        {
+            _fkProducto = a;
+            _fkDetalle = b;
+            _descripcion = c;
+        }
+
+        public DatosProducto()
+        {
+
+        }
+    }
+}
